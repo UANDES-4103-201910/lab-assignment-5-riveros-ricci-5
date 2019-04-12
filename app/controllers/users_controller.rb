79fcb7b14c463823312cbe19f6be7b7c93b5adc0
Pedro <priccis@gmail.com>
2019-04-11 21:07:38 -0400

    some implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
   def create
-    # TODO
+    @user = User.new(params.fetch(:user, {}))
+    render json: @user
   end
 
   def update
@@ -8,7 +15,8 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    # TODO
+    @user.destroy
+    render js: "alert('Hello Rails');"
   end
 
   def user_with_most_tickets
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: %i[create destroy update]
+  resources :users #, only: %i[create destroy update]
   resources :events, only: %i[create destroy update]
   resources :event_venues, only: %i[create destroy update]
   resources :tickets, only: %i[create destroy update]
